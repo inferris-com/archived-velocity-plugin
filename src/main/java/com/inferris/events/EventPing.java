fff88ab4438d0a1ca254003eeb3fe0b62b229894
Azzy <45441561+Refrizor@users.noreply.github.com>
2023-05-22 02:33:16 -0400

    Vanish online rebalance

--- src/main/java/com/inferris/events/EventPing.java
+++ src/main/java/com/inferris/events/EventPing.java
@@ -1,14 +1,10 @@
 package com.inferris.events;
 
-import com.inferris.Inferris;
-import com.inferris.player.PlayerDataManager;
 import com.inferris.player.registry.Registry;
 import com.inferris.player.registry.RegistryManager;
 import com.inferris.player.vanish.VanishState;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.ServerPing;
-import net.md_5.bungee.api.chat.TextComponent;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
