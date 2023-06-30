7e5dc97b7abd1354e970930158d4d6cf24b9f917
Azzy <45441561+Refrizor@users.noreply.github.com>
2023-06-29 21:23:51 -0400

    Updating javadocs

--- src/main/java/com/inferris/player/PlayerData.java
+++ src/main/java/com/inferris/player/PlayerData.java
@@ -1,14 +1,12 @@
 package com.inferris.player;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.inferris.player.registry.Registry;
 import com.inferris.rank.Branch;
 import com.inferris.rank.Rank;
 import com.inferris.rank.RankRegistry;
 import com.inferris.rank.RanksManager;
 import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -45,9 +43,24 @@ public class PlayerData implements Serializable {
         return rank.getBranchID(branch);
     }
 
+    /**
+     * Sets the rank of a player for a specific branch.
+     *
+     * @param branch the branch for which the rank should be set
+     * @param level  the level of the rank to set
+     * @since 1.0
+     */
+
     public void setRank(Branch branch, int level) {
         RanksManager.getInstance().setRank(ProxyServer.getInstance().getPlayer(registry.getUuid()), branch, level);
     }
+
+    /**
+     * Returns a list of {@link RankRegistry} objects based on the branches of the player's rank.
+     * The returned list includes all applicable branches based on the player's rank.
+     * @return A list of RankRegistry objects representing the branches of the player's rank.
+     * @since 1.0
+     */
 
     @JsonIgnore
     public List<RankRegistry> getByBranches() {
@@ -66,8 +79,15 @@ public class PlayerData implements Serializable {
         }
         return ranks;
     }
-    @JsonIgnore
 
+    /**
+     * Returns the {@link RankRegistry} associated with the highest branch of the player's rank.
+     * The returned RankRegistry represents the highest level of authority or privilege based on the player's rank.
+     * @return The RankRegistry associated with the highest branch of the player's rank.
+     * @since 1.0
+     */
+
+    @JsonIgnore
     public RankRegistry getByBranch() {
         int staff = getBranchValue(Branch.STAFF);
         int donor = getBranchValue(Branch.DONOR);
