0df2f378f7f4d8e4abd5858a958b0eb7e5ef75bb
Azzy <45441561+Refrizor@users.noreply.github.com>
2023-07-02 15:21:04 -0400

    Profiles!

--- src/main/java/com/inferris/commands/CommandProfile.java
+++ src/main/java/com/inferris/commands/CommandProfile.java
@@ -41,28 +41,101 @@ public class CommandProfile extends Command implements TabExecutor {
                 sendPlayerProfile(player, player);
             }
             if (length == 1) {
+                if (args[0].equalsIgnoreCase("set")) {
+                    player.sendMessage(new TextComponent(ChatColor.RESET + "Options"));
+                    player.sendMessage(new TextComponent(ChatColor.YELLOW + "/profile set pronouns <pronouns>"));
+                    player.sendMessage(new TextComponent(ChatColor.YELLOW + "/profile set bio <bio>"));
+                    return;
+                } else if (args[0].equalsIgnoreCase("unset")) {
+                    player.sendMessage(new TextComponent(ChatColor.RESET + "Options"));
+                    player.sendMessage(new TextComponent(ChatColor.YELLOW + "/profile unset pronouns"));
+                    player.sendMessage(new TextComponent(ChatColor.YELLOW + "/profile unset bio"));
+                    return;
+                }
                 ProxiedPlayer target = ProxyServer.getInstance().getPlayer(args[0]);
                 sendPlayerProfile(player, target);
             }
-            if (length >= 2) {
-                if (args[0].equalsIgnoreCase("set")) {
-                    String bio = String.join(" ", Arrays.copyOfRange(args, 1, length));
-                    String sql = "UPDATE profile SET bio = ? WHERE uuid = ?";
-                    Object[] parameters = {bio, player.getUniqueId().toString()};
-                    bio = ChatColor.translateAlternateColorCodes('&', bio.replace("\\n", "\n"));
+            if (length == 2) {
+                if (args[0].equalsIgnoreCase("unset")) {
+                    if (args[1].equalsIgnoreCase("bio") || args[1].equalsIgnoreCase("pronouns")) {
+                        String fieldToUnset = args[1].toLowerCase();
+                        String sql = "UPDATE profile SET " + fieldToUnset + " = ? WHERE uuid = ?";
+                        Object[] parameters = {null, player.getUniqueId().toString()};
 
-                    try {
-                        DatabaseUtils.executeUpdate(sql, parameters);
-                        player.sendMessage(new TextComponent(ChatColor.GREEN + "Your bio has been successfully updated"));
-                        playerData.getProfile().setBio(bio);
-                        PlayerDataManager.getInstance().updateAllData(player, playerData);
-                        try (Jedis jedis = Inferris.getJedisPool().getResource()) {
-                            jedis.publish(JedisChannels.PLAYERDATA_CACHE_UPDATE.name(), CacheSerializationUtils.serializePlayerData(playerData));
+                        try {
+                            DatabaseUtils.executeUpdate(sql, parameters);
+                            player.sendMessage(new TextComponent(ChatColor.GREEN + "Your " + fieldToUnset + " has been cleared."));
+                            if (fieldToUnset.equals("bio")) {
+                                playerData.getProfile().setBio(null);
+                            } else if (fieldToUnset.equals("pronouns")) {
+                                playerData.getProfile().setPronouns(null);
+                            }
+
+                            PlayerDataManager.getInstance().updateAllData(player, playerData);
+                            try (Jedis jedis = Inferris.getJedisPool().getResource()) {
+                                jedis.publish(JedisChannels.PLAYERDATA_CACHE_UPDATE.name(), CacheSerializationUtils.serializePlayerData(playerData));
+                            }
+                        } catch (SQLException | JsonProcessingException e) {
+                            throw new RuntimeException(e);
                         }
-                    } catch (SQLException | JsonProcessingException e) {
-                        player.sendMessage(new TextComponent(ChatColor.RED + "Uh oh, something went wrong while you were setting your bio."));
-                        player.sendMessage(new TextComponent(ChatColor.RED + "Here's the stacktrace: " + ChatColor.RESET + ChatColor.ITALIC + e.getMessage()));
-                        e.printStackTrace();
+                    }
+                }
+            }
+            if (length >= 3) {
+                if (args[0].equalsIgnoreCase("set")) {
+                    if (args[1].equalsIgnoreCase("bio")) {
+                        String bio = String.join(" ", Arrays.copyOfRange(args, 2, length));
+                        String sql = "UPDATE profile SET bio = ? WHERE uuid = ?";
+                        Object[] parameters = {bio, player.getUniqueId().toString()};
+                        bio = ChatColor.translateAlternateColorCodes('&', bio.replace("\\n", "\n"));
+
+                        try {
+                            DatabaseUtils.executeUpdate(sql, parameters);
+                            player.sendMessage(new TextComponent(ChatColor.GREEN + "Your bio has been successfully updated"));
+                            playerData.getProfile().setBio(bio);
+                            PlayerDataManager.getInstance().updateAllData(player, playerData);
+                            try (Jedis jedis = Inferris.getJedisPool().getResource()) {
+                                jedis.publish(JedisChannels.PLAYERDATA_CACHE_UPDATE.name(), CacheSerializationUtils.serializePlayerData(playerData));
+                            }
+                        } catch (SQLException | JsonProcessingException e) {
+                            player.sendMessage(new TextComponent(ChatColor.RED + "Uh oh, something went wrong while you were setting your bio."));
+                            player.sendMessage(new TextComponent(ChatColor.RED + "Here's the stacktrace: " + ChatColor.RESET + ChatColor.ITALIC + e.getMessage()));
+                            e.printStackTrace();
+                        }
+                    } else if (args[1].equalsIgnoreCase("pronouns")) {
+                        String pronouns = String.join(" ", Arrays.copyOfRange(args, 2, length));
+                        pronouns = pronouns.replace(" ", "");
+                        boolean isValid = pronouns.matches("[a-zA-Z/]+");
+
+
+                        if (!pronouns.contains("/")) {
+                            player.sendMessage(new TextComponent(ChatColor.RED + "You must include a forward slash. Example: they/them"));
+                            return;
+                        }
+                        if (pronouns.length() > 10) {
+                            player.sendMessage(new TextComponent(ChatColor.RED + "Pronouns should be within 10 characters."));
+                            return;
+                        }
+                        String sql = "UPDATE profile SET pronouns = ? WHERE uuid = ?";
+                        Object[] parameters = {pronouns, player.getUniqueId().toString()};
+                        if (isValid) {
+
+                            try {
+                                DatabaseUtils.executeUpdate(sql, parameters);
+                                player.sendMessage(new TextComponent(ChatColor.GREEN + "Your pronouns have been successfully updated"));
+                                playerData.getProfile().setPronouns(pronouns);
+                                PlayerDataManager.getInstance().updateAllData(player, playerData);
+                                try (Jedis jedis = Inferris.getJedisPool().getResource()) {
+                                    jedis.publish(JedisChannels.PLAYERDATA_CACHE_UPDATE.name(), CacheSerializationUtils.serializePlayerData(playerData));
+                                }
+                            } catch (SQLException | JsonProcessingException e) {
+                                player.sendMessage(new TextComponent(ChatColor.RED + "Uh oh, something went wrong while you were setting your pronouns."));
+                                player.sendMessage(new TextComponent(ChatColor.RED + "Here's the stacktrace: " + ChatColor.RESET + ChatColor.ITALIC + e.getMessage()));
+                                e.printStackTrace();
+                            }
+                        } else {
+                            player.sendMessage(new TextComponent(ChatColor.RED + "Invalid pronouns format. Please only use letters (a-z, A-Z) and slash (/), and keep it within 10 characters."));
+                        }
                     }
                 }
             }
@@ -71,21 +144,47 @@ public class CommandProfile extends Command implements TabExecutor {
 
     @Override
     public Iterable<String> onTabComplete(CommandSender sender, String[] args) {
-        if (args.length == 1 && sender instanceof ProxiedPlayer player) {
-            String partialPlayerName = args[0];
-            List<String> playerNames = new ArrayList<>();
-            for (ProxiedPlayer proxiedPlayers : ProxyServer.getInstance().getPlayers()) {
-                if (!(PlayerDataManager.getInstance().getPlayerData(proxiedPlayers).getVanishState() == VanishState.ENABLED)) {
-                    String playerName = proxiedPlayers.getName();
-                    if (playerName.toLowerCase().startsWith(partialPlayerName.toLowerCase())) {
-                        playerNames.add(playerName);
+        if (sender instanceof ProxiedPlayer player) {
+            if (args.length == 1) {
+                String partialPlayerName = args[0];
+                List<String> completions = new ArrayList<>();
+
+                // Add "set" option to the completions list
+                if ("set".startsWith(partialPlayerName.toLowerCase())) {
+                    completions.add("set");
+                }
+                if ("unset".startsWith(partialPlayerName.toLowerCase())) {
+                    completions.add("unset");
+                }
+
+                for (ProxiedPlayer proxiedPlayers : ProxyServer.getInstance().getPlayers()) {
+                    if (!(PlayerDataManager.getInstance().getPlayerData(proxiedPlayers).getVanishState() == VanishState.ENABLED)) {
+                        String playerName = proxiedPlayers.getName();
+                        if (playerName.toLowerCase().startsWith(partialPlayerName.toLowerCase())) {
+                            completions.add(playerName);
+                        }
                     }
                 }
+                return completions;
+            }else if(args.length == 2) {
+                if (args[0].equalsIgnoreCase("set") || args[0].equalsIgnoreCase("unset")) {
+                    String partialOption = args[1].toLowerCase();
+                    List<String> options = new ArrayList<>();
+
+                    List<String> availableOptions = Arrays.asList("pronouns", "bio");
+
+                    for (String option : availableOptions) {
+                        if (option.toLowerCase().startsWith(partialOption)) {
+                            options.add(option);
+                        }
+                    }
+                    return options;
+                }
             }
-            return playerNames;
         }
         return Collections.emptyList();
     }
+
 
     private void sendPlayerProfile(ProxiedPlayer player, ProxiedPlayer target) {
         PlayerData playerData = PlayerDataManager.getInstance().getPlayerData(target);
@@ -95,15 +194,17 @@ public class CommandProfile extends Command implements TabExecutor {
         player.sendMessage(new TextComponent(ChatColor.YELLOW + "Profile of " + playerData.getNameColor() + target.getName()));
         player.sendMessage(new TextComponent(""));
         player.sendMessage(new TextComponent(ChatColor.GRAY + "Rank: " + playerData.getByBranch().getPrefix()));
-        player.sendMessage(new TextComponent(ChatColor.GRAY + "Pronouns: " + reset + profile.getPronouns()));
+        if (profile.getPronouns() != null)
+            player.sendMessage(new TextComponent(ChatColor.GRAY + "Pronouns: " + reset + profile.getPronouns()));
         player.sendMessage(new TextComponent(ChatColor.GRAY + "Registration date: " + reset + getFormattedRegistrationDate(profile.getRegistrationDate())));
+        if (profile.getBio() != null)
+            player.sendMessage(new TextComponent(ChatColor.GRAY + "Bio: " + reset + profile.getBio()));
         player.sendMessage(new TextComponent(""));
-        player.sendMessage(new TextComponent(ChatColor.GRAY + "Bio: " + reset + profile.getBio()));
 
     }
 
     private String getFormattedRegistrationDate(LocalDate registrationDate) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy"); // Define the desired format pattern
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd, yyyy"); // Define the desired format pattern
         return registrationDate.format(formatter); // Format the registration date using the formatter
     }
 }
