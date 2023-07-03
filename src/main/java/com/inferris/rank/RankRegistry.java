1b45f35d95faa7868f61867feb39ab528b0d04bb
Azzy <45441561+Refrizor@users.noreply.github.com>
2023-07-02 20:49:37 -0400

    New color field

--- src/main/java/com/inferris/rank/RankRegistry.java
+++ src/main/java/com/inferris/rank/RankRegistry.java
@@ -3,20 +3,22 @@ package com.inferris.rank;
 import net.md_5.bungee.api.ChatColor;
 
 public enum RankRegistry {
-    ADMIN(ChatColor.RED + "[Admin]", Branch.STAFF, 3),
-    MOD(ChatColor.DARK_GREEN + "[Mod]", Branch.STAFF, 2),
-    HELPER(ChatColor.BLUE + "[Helper]", Branch.STAFF, 1),
-    DONOR(ChatColor.AQUA + "[Backer]", Branch.DONOR, 1),
-    NONE(String.valueOf(ChatColor.RESET), null, 0);
+    ADMIN(ChatColor.RED + "[Admin]", Branch.STAFF, 3, ChatColor.RED),
+    MOD(ChatColor.DARK_GREEN + "[Mod]", Branch.STAFF, 2, ChatColor.DARK_GREEN),
+    HELPER(ChatColor.BLUE + "[Helper]", Branch.STAFF, 1, ChatColor.BLUE),
+    DONOR(ChatColor.AQUA + "[Backer]", Branch.DONOR, 1, ChatColor.AQUA),
+    NONE(String.valueOf(ChatColor.RESET), null, 0, ChatColor.RESET);
 
     private final String prefix;
     private final Branch branch;
     private final int level;
+    private final ChatColor color;
 
-    RankRegistry(String prefix, Branch branch, int level) {
+    RankRegistry(String prefix, Branch branch, int level, ChatColor color) {
         this.prefix = prefix;
         this.branch = branch;
         this.level = level;
+        this.color = color;
     }
 
     public String getPrefix() {
@@ -36,5 +38,9 @@ public enum RankRegistry {
 
     public int getLevel() {
         return level;
+    }
+
+    public ChatColor getColor() {
+        return color;
     }
 }
