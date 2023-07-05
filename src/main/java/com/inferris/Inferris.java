2e436f40feb853a4a65aa9f1eb3a93d9c3a6f9b7
Azzy <45441561+Refrizor@users.noreply.github.com>
2023-07-05 18:00:21 -0400

    Renamed Friend command

--- src/main/java/com/inferris/Inferris.java
+++ src/main/java/com/inferris/Inferris.java
@@ -62,7 +62,7 @@ public class Inferris extends Plugin {
         pluginManager.registerCommand(this, new CommandViewlogs("viewlogs"));
         pluginManager.registerCommand(this, new CommandReport("report"));
         pluginManager.registerCommand(this, new CommandLocate("locate"));
-        pluginManager.registerCommand(this, new CommandFriend("friends"));
+        pluginManager.registerCommand(this, new CommandFriend("friend"));
 
         getProxy().registerChannel(BungeeChannel.STAFFCHAT.getName());
         getProxy().registerChannel(BungeeChannel.PLAYER_REGISTRY.getName());
