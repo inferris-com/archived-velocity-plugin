dcd49b081ecd5ba821d32be2637a159eba4cd7b9
Azzy <45441561+Refrizor@users.noreply.github.com>
2023-09-07 04:26:18 -0400

    Tab complete

--- src/main/java/com/inferris/commands/CommandFriend.java
+++ src/main/java/com/inferris/commands/CommandFriend.java
@@ -5,16 +5,19 @@ import com.inferris.player.PlayerData;
 import com.inferris.player.PlayerDataManager;
 import com.inferris.player.friends.Friends;
 import com.inferris.player.friends.FriendsManager;
+import com.inferris.player.vanish.VanishState;
+import com.inferris.rank.Branch;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.plugin.TabExecutor;
 
-import java.util.UUID;
+import java.util.*;
 
-public class CommandFriend extends Command {
+public class CommandFriend extends Command implements TabExecutor {
     public CommandFriend(String name) {
         super(name);
     }
@@ -56,7 +59,7 @@ public class CommandFriend extends Command {
                 FriendsManager friendsManager = FriendsManager.getInstance();
                 UUID playerUUID = player.getUniqueId();
 
-                if(args[0].equalsIgnoreCase("list")){
+                if (args[0].equalsIgnoreCase("list")) {
                     int index = Integer.parseInt(args[1]);
                     friendsManager.listFriends(player.getUniqueId(), index);
                     return;
@@ -88,10 +91,43 @@ public class CommandFriend extends Command {
                     }
                 } else if (args[0].equalsIgnoreCase("remove")) {
                     FriendsManager.getInstance().removeFriend(playerUUID, targetUUID);
-                }else if(args[0].equalsIgnoreCase("reject") || args[0].equalsIgnoreCase("deny")){
+                } else if (args[0].equalsIgnoreCase("reject") || args[0].equalsIgnoreCase("deny")) {
                     FriendsManager.getInstance().rejectFriendRequest(playerUUID, targetUUID);
                 }
             }
         }
+    }
+
+    @Override
+    public Iterable<String> onTabComplete(CommandSender sender, String[] args) {
+        int length = args.length;
+        if (sender instanceof ProxiedPlayer player) {
+            if (length < 2) {
+                String partialOption = args[0].toLowerCase();
+                List<String> options = new ArrayList<>();
+                List<String> availableOptions = Arrays.asList("add", "accept", "list", "remove", "deny");
+                for (String option : availableOptions) {
+                    if (option.toLowerCase().startsWith(partialOption)) {
+                        options.add(option);
+                    }
+                }
+                return options;
+            }
+            if (length == 2) {
+                String partialPlayerName = args[0];
+                List<String> playerNames = new ArrayList<>();
+                PlayerData playerData = PlayerDataManager.getInstance().getPlayerData(player);
+                for (ProxiedPlayer proxiedPlayers : ProxyServer.getInstance().getPlayers()) {
+                    if (PlayerDataManager.getInstance().getPlayerData(proxiedPlayers).getVanishState() == VanishState.DISABLED || playerData.getBranchValue(Branch.STAFF) >= 3) {
+                        String playerName = proxiedPlayers.getName();
+                        if (playerName.toLowerCase().startsWith(partialPlayerName.toLowerCase())) {
+                            playerNames.add(playerName);
+                        }
+                    }
+                }
+                return playerNames;
+            }
+        }
+        return Collections.emptyList();
     }
 }
