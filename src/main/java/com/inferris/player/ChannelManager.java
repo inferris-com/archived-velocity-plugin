f020c62dd2f77c888aaa2c96603dfb77b52f13f2
Refrizor <retizao.tv@gmail.com>
2024-05-11 20:51:34 -0400

    Add database functionality to ChannelManager

--- src/main/java/com/inferris/player/ChannelManager.java
+++ src/main/java/com/inferris/player/ChannelManager.java
@@ -1,8 +1,12 @@
 package com.inferris.player;
 
+import com.inferris.Inferris;
+import com.inferris.util.DatabaseUtils;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
+
+import java.sql.SQLException;
 
 public class ChannelManager {
 
@@ -19,6 +23,14 @@ public class ChannelManager {
             }
             player.sendMessage(new TextComponent(ChatColor.YELLOW + "Channel set to " + channelName));
         }
+
+        String sql = "UPDATE player_data SET channel = ? WHERE uuid = ?";
+        Object[] parameters = {channel.toString(), player.getUniqueId().toString()};
+            try {
+                DatabaseUtils.executeUpdate(sql, parameters);
+            }catch(SQLException e){
+                Inferris.getInstance().getLogger().severe(e.getMessage());
+            }
 
         PlayerDataManager.getInstance().updateAllDataAndPush(player, playerData);
     }
