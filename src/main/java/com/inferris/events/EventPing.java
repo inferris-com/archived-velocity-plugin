2aa659d2c3e63ad377820eda8a4d844e53104dcf
Refrizor <retizao.tv@gmail.com>
2024-05-12 12:07:40 -0400

    Fix EventPing to include vanish compatibility

--- src/main/java/com/inferris/events/EventPing.java
+++ src/main/java/com/inferris/events/EventPing.java
@@ -1,10 +1,14 @@
 package com.inferris.events;
 
 import com.inferris.Inferris;
+import com.inferris.player.PlayerData;
+import com.inferris.player.PlayerDataManager;
+import com.inferris.player.vanish.VanishState;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -20,11 +24,10 @@ public class EventPing implements Listener {
         serverPing.setDescriptionComponent(new TextComponent(ChatColor.translateAlternateColorCodes('&', Inferris.getProperties().getProperty("server.list.motd"))));
 
         if(count >=1){
-            int vanishedCount = getTotalVanishedPlayers();
 
-            count = count - vanishedCount;
+            count = count - getTotalVanishedPlayers();
 
-            serverPing.getPlayers().setOnline(vanishedCount);
+            serverPing.getPlayers().setOnline(count);
 
             event.setResponse(serverPing);
         }
@@ -32,7 +35,13 @@ public class EventPing implements Listener {
 
     // TODO: Vanish count
     private int getTotalVanishedPlayers() {
-        int count = 0;
-        return 0;
+        int vanishedCount = 0;
+        for(ProxiedPlayer player : ProxyServer.getInstance().getPlayers()){
+            PlayerData playerData = PlayerDataManager.getInstance().getPlayerData(player);
+            if(playerData.getVanishState() == VanishState.ENABLED){
+                vanishedCount++;
+            }
+        }
+        return vanishedCount;
     }
 }
