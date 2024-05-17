a8dcb95db9d2f3367e81418c39a686b6929cd508
Refrizor <retizao.tv@gmail.com>
2024-05-17 17:49:38 -0400

    Remove client-viewable debug messages

--- src/main/java/com/inferris/events/JedisReceive.java
+++ src/main/java/com/inferris/events/JedisReceive.java
@@ -50,7 +50,9 @@ public class JedisReceive extends JedisPubSub {
         }
 
         if (channel.equals(JedisChannels.STAFFCHAT.getChannelName())) {
-            Inferris.getInstance().getLogger().severe("Triggered");
+            if (ServerStateManager.getCurrentState() == ServerState.DEBUG)
+                Inferris.getInstance().getLogger().severe("Triggered");
+
             StaffChatMessage staffChatMessage = StaffChatSerializer.deserialize(message);
             assert staffChatMessage != null;
 
@@ -69,65 +71,40 @@ public class JedisReceive extends JedisPubSub {
             }
         }
 
-        if (channel.equalsIgnoreCase(JedisChannels.VIEW_LOGS_SPIGOT_TO_PROXY.getChannelName())) {
-            Inferris.getInstance().getLogger().warning("JedisReceive triggered"); // Logging for debugging
-            Inferris.getInstance().getLogger().warning("Executing processMessage: " + message); // Logging for debugging
+        if (channel.equalsIgnoreCase(JedisChannels.VIEW_LOGS_SPIGOT_TO_PROXY.getChannelName()))
+            if (ServerStateManager.getCurrentState() == ServerState.DEBUG)
+                Inferris.getInstance().getLogger().warning("Executing processMessage: " + message); // Logging for debugging
+        String[] parts = message.split(":");
+        UUID uuid = UUID.fromString(parts[0]);
+        String json = message.substring(parts[0].length() + 1);
 
-            String[] parts = message.split(":");
-            UUID uuid = UUID.fromString(parts[0]);
-            String json = message.substring(parts[0].length() + 1);
+        PlayerData playerData = PlayerDataManager.getInstance().getRedisData(uuid);
+        ProxiedPlayer player = ProxyServer.getInstance().getPlayer(playerData.getUuid());
 
-            PlayerData playerData = PlayerDataManager.getInstance().getRedisData(uuid);
-            ProxiedPlayer player = ProxyServer.getInstance().getPlayer(playerData.getUuid());
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            List<String> chatMessages = objectMapper.readValue(json, new TypeReference<List<String>>() {
+            });
 
-            ObjectMapper objectMapper = new ObjectMapper();
-            try {
-                List<String> chatMessages = objectMapper.readValue(json, new TypeReference<List<String>>() {
-                });
-                Inferris.getInstance().getLogger().warning("Executing processMessage: " + chatMessages.size());
-
-                if (chatMessages.isEmpty()) {
-                    player.sendMessage(new TextComponent(ChatColor.RED + "No chat log messages available."));
-                } else {
-                    player.sendMessage("--------------------------------------------------------------");
-                    for (String chatMessage : chatMessages) {
-                        int startIndex = chatMessage.indexOf("] ") + 2;
-                        String timestamp = chatMessage.substring(0, startIndex);
-                        String messageContent = chatMessage.substring(startIndex);
-                        player.sendMessage(new TextComponent(timestamp + messageContent));
-                    }
+            if (chatMessages.toString().equalsIgnoreCase("[]")) {
+                player.sendMessage(new TextComponent(ChatColor.RED + "No chat log messages available."));
+            } else {
+                player.sendMessage();
+                for (String chatMessage : chatMessages) {
+                    int startIndex = chatMessage.indexOf("] ") + 2;
+                    String timestamp = chatMessage.substring(0, startIndex);
+                    String messageContent = chatMessage.substring(startIndex);
+                    player.sendMessage(new TextComponent(timestamp + messageContent));
                 }
-
-                if (ServerStateManager.getCurrentState() == ServerState.DEBUG) {
-                    Inferris.getInstance().getLogger().severe("================================");
-                    Inferris.getInstance().getLogger().severe("Event has been received");
-                    Inferris.getInstance().getLogger().severe("================================");
-                    ServerUtil.broadcastMessage(json);
-                    ServerUtil.broadcastMessage(String.valueOf(uuid));
-                }
-            } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
             }
-        }
-    }
 
-    @Deprecated
-    private void handleReportPayload(ReportPayload reportPayload) {
-
-        for (ProxiedPlayer staffPlayer : ProxyServer.getInstance().getPlayers()) {
-            // Check if the player is a staff member
-            if (PlayerDataManager.getInstance().getPlayerData(staffPlayer).isStaff()) {
-                staffPlayer.sendMessage(new TextComponent(Tags.STAFF.getName(true) + ChatColor.RED + "New chat report!"));
-                staffPlayer.sendMessage(new TextComponent(""));
-
-                // Send the report information to the staff player
-                staffPlayer.sendMessage(new TextComponent(ChatColor.GRAY + "Reported Player: " + ChatColor.YELLOW + reportPayload.getReported()));
-                staffPlayer.sendMessage(new TextComponent(ChatColor.GRAY + "Reported by: " + ChatColor.RESET + reportPayload.getSender()));
-                staffPlayer.sendMessage(new TextComponent(""));
-
-                staffPlayer.sendMessage(new TextComponent("Reason: " + ChatColor.YELLOW + reportPayload.getReason()));
-                staffPlayer.sendMessage(new TextComponent("Server: " + ChatColor.YELLOW + reportPayload.getServer()));
+            if (ServerStateManager.getCurrentState() == ServerState.DEBUG) {
+                Inferris.getInstance().getLogger().severe("================================");
+                Inferris.getInstance().getLogger().severe("[Bungee] Viewlog Event has been received");
+                Inferris.getInstance().getLogger().severe("================================");
             }
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
         }
     }
 }
