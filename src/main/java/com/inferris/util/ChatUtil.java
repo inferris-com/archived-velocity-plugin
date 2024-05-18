682ee56398398d37c1dd264e22a3554ce8ed313d
Refrizor <retizao.tv@gmail.com>
2024-05-18 17:17:48 -0400

    Add chat style modifiers

--- src/main/java/com/inferris/util/ChatUtil.java
+++ src/main/java/com/inferris/util/ChatUtil.java
@@ -32,6 +32,30 @@ public class ChatUtil {
         return ChatColor.translateAlternateColorCodes('&', message);
     }
 
+    public static String translateToHex(String message, FormatType type) {
+        final Pattern pattern = Pattern.compile("(?<!\\\\)(#[a-fA-F0-9]{6})");
+        Matcher matcher = pattern.matcher(message);
+        while (matcher.find()) {
+            String hexCode = message.substring(matcher.start(), matcher.end());
+            String replaceSharp = hexCode.replace('#', 'x');
+
+            char[] ch = replaceSharp.toCharArray();
+            StringBuilder builder = new StringBuilder();
+            for (char c : ch) {
+                builder.append("&").append(c);
+            }
+            switch (type) {
+                case BOLD -> builder.append(ChatColor.BOLD);
+                case ITALICS -> builder.append(ChatColor.ITALIC);
+            }
+            builder.append(ChatColor.BOLD);
+
+            message = message.replace(hexCode, builder.toString());
+            matcher = pattern.matcher(message);
+        }
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
+
     public static TextComponent createTextComponent(String prefix, String content) {
         TextComponent component = new TextComponent(prefix);
         component.addExtra(content);
@@ -45,6 +69,13 @@ public class ChatUtil {
         return component;
     }
 
+    public static TextComponent createLegacyClickableTextComponent(String prefix, String hoverText, String command, ClickEvent.Action action) {
+        TextComponent component = new TextComponent(TextComponent.fromLegacyText(prefix));
+        component.setHoverEvent(createHoverEvent(hoverText));
+        component.setClickEvent(createClickEvent(command, action));
+        return component;
+    }
+
     public static HoverEvent createHoverEvent(String text) {
         return new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(text));
     }
@@ -52,4 +83,10 @@ public class ChatUtil {
     public static ClickEvent createClickEvent(String value, ClickEvent.Action action) {
         return new ClickEvent(action, value);
     }
+
+    public enum FormatType {
+        BOLD,
+        ITALICS;
+    }
 }
+
