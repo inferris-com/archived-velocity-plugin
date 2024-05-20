54f3148d305e59120ca0f774429de311985f723f
Refrizor <retizao.tv@gmail.com>
2024-05-20 19:36:56 -0400

    Add ColorType for common color retrieval

---
+++ src/main/java/com/inferris/common/ColorType.java
@@ -0,0 +1,21 @@
+package com.inferris.common;
+
+import java.awt.*;
+
+public enum ColorType {
+    HEADER(new Color(106, 137, 252)),
+    HEADER2(new Color(30, 144, 255)),
+    BRAND_PRIMARY(new Color(110, 69, 226)),
+    BRAND_SECONDARY(new Color(0, 191, 255)),
+    LUMINA(new Color(174, 187, 235));
+
+    private final Color color;
+    ColorType(Color color){
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+}
+
