f2deaf48a17c67c226c2c4b94234202d31b009b3
Refrizor <retizao.tv@gmail.com>
2024-05-24 23:11:24 -0400

    Better motd

--- src/main/java/com/inferris/events/EventPing.java
+++ src/main/java/com/inferris/events/EventPing.java
@@ -1,13 +1,16 @@
 package com.inferris.events;
 
 import com.inferris.Inferris;
+import com.inferris.common.ColorType;
 import com.inferris.config.ConfigType;
+import com.inferris.config.ConfigurationHandler;
 import com.inferris.player.PlayerData;
 import com.inferris.player.PlayerDataManager;
 import com.inferris.player.vanish.VanishState;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.ServerPing;
+import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ProxyPingEvent;
@@ -22,8 +25,20 @@ public class EventPing implements Listener {
         int count = ProxyServer.getInstance().getOnlineCount();
 
         ServerPing serverPing = event.getResponse();
-        serverPing.setDescriptionComponent(new TextComponent(ChatColor.translateAlternateColorCodes('&',
-                Inferris.getInstance().getConfigurationHandler().getProperties(ConfigType.PROPERTIES).getProperty("server.list.motd"))));
+        String motdTemplate = ConfigurationHandler.getInstance().getProperties(ConfigType.PROPERTIES).getProperty("server.list.motd");
+        String motdMessage = motdTemplate;
+        if (motdTemplate.contains("Inferris")) {
+            motdMessage = motdTemplate.replace("Inferris", ChatColor.of(ColorType.BRAND_SECONDARY.getColor()) + "Inferris");
+        }
+
+        BaseComponent[] components = TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', motdMessage));
+        TextComponent descriptionComponent = new TextComponent();
+
+        for (BaseComponent component : components) {
+            descriptionComponent.addExtra(component);
+        }
+
+        serverPing.setDescriptionComponent(descriptionComponent);
 
         if(count >=1){
 
