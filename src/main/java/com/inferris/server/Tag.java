4348c57c898e82d5e04bf85112ed82e09082b378
Refrizor <retizao.tv@gmail.com>
2024-06-07 00:44:46 -0400

    Add POI tag

--- src/main/java/com/inferris/server/Tag.java
+++ src/main/java/com/inferris/server/Tag.java
@@ -3,7 +3,8 @@ package com.inferris.server;
 import net.md_5.bungee.api.ChatColor;
 
 public enum Tag {
-    STAFF(ChatColor.AQUA + "[STAFF]");
+    STAFF(ChatColor.AQUA + "[STAFF]"),
+    POI(ChatColor.RED + "[POI Alert]");
 
     private final String name;
     Tag(String name){
