72956035238f2a446227c1786c17126a2ce196f8
Refrizor <retizao.tv@gmail.com>
2024-06-07 00:44:21 -0400

    Refactor JedisHelper methods to be static

--- src/main/java/com/inferris/server/jedis/JedisHelper.java
+++ src/main/java/com/inferris/server/jedis/JedisHelper.java
@@ -1,18 +1,15 @@
 package com.inferris.server.jedis;
 
+import com.inferris.Inferris;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 public class JedisHelper {
 
-    private final JedisPool jedisPool;
-
-    public JedisHelper(JedisPool jedisPool) {
-        this.jedisPool = jedisPool;
-    }
+    private static final JedisPool jedisPool = Inferris.getJedisPool();
 
     // Method to easily perform an HSET operation
-    public boolean hsetWithStatus(String key, String field, String value) {
+    public static boolean hsetWithStatus(String key, String field, String value) {
         try (Jedis jedis = jedisPool.getResource()) {
             jedis.hset(key, field, value);
             return true;
@@ -23,7 +20,7 @@ public class JedisHelper {
         }
     }
 
-    public void hset(String key, String field, String value) {
+    public static void hset(String key, String field, String value) {
         try (Jedis jedis = jedisPool.getResource()) {
             jedis.hset(key, field, value);
         } catch (Exception e) {
@@ -32,8 +29,8 @@ public class JedisHelper {
         }
     }
 
-    public void publish(JedisChannels channel, String message){
-        try(Jedis jedis = jedisPool.getResource()){
+    public static void publish(JedisChannels channel, String message) {
+        try (Jedis jedis = jedisPool.getResource()) {
             jedis.publish(channel.getChannelName(), message);
         }
     }
