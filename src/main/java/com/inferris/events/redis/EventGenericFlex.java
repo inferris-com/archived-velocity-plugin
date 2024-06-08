0ea643d378288c88ffb8132e24ac630d873bd233
Refrizor <retizao.tv@gmail.com>
2024-06-08 02:16:04 -0400

    Add fetchOnlineVisiblePlayers check to pub back information

--- src/main/java/com/inferris/events/redis/EventGenericFlex.java
+++ src/main/java/com/inferris/events/redis/EventGenericFlex.java
@@ -1,12 +1,27 @@
 package com.inferris.events.redis;
 
+import com.inferris.Inferris;
 import com.inferris.events.redis.dispatching.JedisEventHandler;
+import com.inferris.server.jedis.JedisChannel;
+import com.inferris.server.jedis.JedisHelper;
 
 public class EventGenericFlex implements JedisEventHandler {
     @Override
     public void handle(String message, String senderId) {
         EventPayload payload = EventPayload.fromPayloadString(message);
-        if (senderId.equals(payload.getSenderId())) {
+        String data = payload.getData();
+
+        if (senderId.equals(Inferris.getInstanceId())) {
+            return;
+        }
+        switch ((GenericAction) payload.getAction()) {
+            case INFORMATION -> {
+                if (data.equals("fetchOnlineVisiblePlayers")) {
+                    JedisHelper.publish(JedisChannel.GENERIC_FLEX_EVENT, new EventPayload(payload.getUuid(),
+                            GenericAction.INFORMATION,
+                            "fetchOnlineVisiblePlayers:return:" + Inferris.getInstance().getVisibleOnlinePlayers(), Inferris.getInstanceId()).toPayloadString());
+                }
+            }
         }
     }
 }
