5266d8d7f40c09280ab5b81d800610cd60b0aa15
Refrizor <retizao.tv@gmail.com>
2024-06-07 23:23:36 -0400

    Add CommandHelp

---
+++ src/main/java/com/inferris/commands/CommandHelp.java
@@ -0,0 +1,15 @@
+package com.inferris.commands;
+
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.plugin.Command;
+
+public class CommandHelp extends Command {
+    public CommandHelp(String name) {
+        super(name);
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] strings) {
+
+    }
+}
