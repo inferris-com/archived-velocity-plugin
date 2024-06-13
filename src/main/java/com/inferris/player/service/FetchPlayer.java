917aea62c6316eb3c446560928435101099f9452
Refrizor <retizao.tv@gmail.com>
2024-06-12 21:17:35 -0400

    Fix command to use repository

--- src/main/java/com/inferris/player/service/FetchPlayer.java
+++ src/main/java/com/inferris/player/service/FetchPlayer.java
@@ -1,32 +1,20 @@
-package com.inferris.player;
+package com.inferris.player.service;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.inferris.Inferris;
 import com.inferris.database.DatabasePool;
 import com.inferris.database.Table;
-import com.inferris.player.vanish.VanishState;
-import com.inferris.rank.Rank;
-import com.inferris.rank.RanksManager;
-import com.inferris.server.Server;
+import com.inferris.player.PlayerData;
 import com.inferris.util.DatabaseUtils;
-import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
 import redis.clients.jedis.Jedis;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.UUID;
 
 public class FetchPlayer {
-    private final PlayerDataService playerDataService;
-    private final PlayerDataRepository playerDataRepository;
-    public FetchPlayer(PlayerDataService playerDataService, PlayerDataRepository playerDataRepository){
-        this.playerDataService = playerDataService;
-        this.playerDataRepository = playerDataRepository;
-    }
 
     /**
      * Retrieves the UUID associated with a given username from player data stored in Redis.
@@ -52,7 +40,7 @@ public class FetchPlayer {
                     ResultSet resultSet = DatabaseUtils.queryData(connection, Table.PLAYER_DATA.getName(), new String[]{"uuid"}, condition);
                     if (resultSet.next()) {
                         uuid = UUID.fromString(resultSet.getString("uuid"));
-                        playerData = playerDataRepository.getPlayerDataFromDatabase(uuid);
+                        playerData = PlayerDataManager.getInstance().getPlayerDataRepository().getPlayerDataFromDatabase(uuid);
                     }
                 } catch (SQLException e) {
                     Inferris.getInstance().getLogger().severe(e.getMessage());
