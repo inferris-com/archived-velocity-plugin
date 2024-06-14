00c5f02937ef7e0fe2b882bdc9e8d65f92238406
Refrizor <retizao.tv@gmail.com>
2024-06-14 13:12:44 -0400

    Add new ColorType

--- src/main/java/com/inferris/common/ColorType.java
+++ src/main/java/com/inferris/common/ColorType.java
@@ -7,7 +7,7 @@ public enum ColorType {
     HEADER2(new Color(30, 144, 255)),
     BRAND_PRIMARY(new Color(110, 69, 226)),
     BRAND_SECONDARY(new Color(0, 191, 255)),
-    LUMINA(new Color(174, 187, 235));
+    LUMINA(new Color(137, 116, 232));
 
     private final Color color;
     ColorType(Color color){
