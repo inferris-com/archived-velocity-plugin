b434223d15216a70d079a239e047000240a2d73f
Refrizor <retizao.tv@gmail.com>
2024-08-21 01:04:04 -0400

    Add ManageContainer for containing manager classes

---
+++ src/main/java/com/inferris/player/service/ManagerContainer.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2024. Inferris.
+ * All rights reserved.
+ */
+
+package com.inferris.player.service;
+
+import com.google.inject.Inject;
+import com.inferris.player.channel.ChannelManager;
+import com.inferris.player.friends.FriendsManager;
+
+public class ManagerContainer {
+    private final PlayerDataManager playerDataManager;
+    private final ChannelManager channelManager;
+    private final CoinsManager coinsManager;
+    private final FriendsManager friendsManager;
+    private final RanksManager ranksManager;
+
+    @Inject
+    public ManagerContainer(PlayerDataManager playerDataManager, RanksManager ranksManager, ChannelManager channelManager, CoinsManager coinsManager, FriendsManager friendsManager) {
+        this.playerDataManager = playerDataManager;
+        this.ranksManager = ranksManager;
+        this.channelManager = channelManager;
+        this.coinsManager = coinsManager;
+        this.friendsManager = friendsManager;
+    }
+
+    public PlayerDataManager getPlayerDataManager() {
+        return playerDataManager;
+    }
+
+    public RanksManager getRanksManager() {
+        return ranksManager;
+    }
+
+    // Provide methods to access or operate on the managers
+    public ChannelManager getChannelManager() {
+        return channelManager;
+    }
+
+    public CoinsManager getCoinsManager() {
+        return coinsManager;
+    }
+
+    public FriendsManager getFriendsManager() {
+        return friendsManager;
+    }
+}
